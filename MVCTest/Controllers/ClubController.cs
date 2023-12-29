75a5cbdff05411d7a29b1d05e9ac584d0a73e68d
EMRE GÜLBAY <20190808071@ogr.akdeniz.edu.tr>
2023-12-29 10:22:08 +0300

    Controller view connections

--- MVCTest/Controllers/ClubController.cs
+++ MVCTest/Controllers/ClubController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCTest.Data;
+using MVCTest.Models;
 
 namespace MVCTest.Controllers
 {
     public class ClubController : Controller
     {
+        private ApplicationDbContext _context;
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
--- MVCTest/Controllers/RaceController.cs
+++ MVCTest/Controllers/RaceController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCTest.Data;
+using MVCTest.Models;
 
 namespace MVCTest.Controllers
 {
     public class RaceController : Controller
     {
+        ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
